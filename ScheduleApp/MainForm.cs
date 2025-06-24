7002c70f51db23de464e47f9d2d1f21c653d848a
ducchinh-code <chinhdd4905@gmail.com>
2025-06-24 22:24:11 +0700

    First commit

---
+++ ScheduleApp/MainForm.cs
@@ -0,0 +1,9 @@
+namespace ScheduleApp;
+
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ScheduleApp/Models.cs
@@ -0,0 +1,25 @@
+﻿namespace ScheduleApp;
+
+public class Models
+{
+    public class Match()
+    {
+        public int Id { get; set; }
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public DateTime Date { get; set; }
+        public string Sport { get; set; }
+        public string League { get; set; }
+        public string Status { get; set; } = "Scheduled";
+        public string Score { get; set; } = "";
+        public string Venue { get; set; } = "";
+    }
+
+    public class Team()
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sport { get; set; }
+        
+    }
+}
